d52003d4d3680976a3cf5d2e4c77c0478baccc9e
Julian Schubert <informatik.schubert@gmail.com>
2021-12-04 17:53:36 +0100

    Day4

---
+++ day-04/task2.py
@@ -0,0 +1,66 @@
+import re
+import sys
+import numpy as np
+
+puzzle_input = []
+
+# Loading puzzle input
+with open("day-04/input.txt") as f:
+    puzzle_input = [line.strip() for line in f.readlines()]
+
+numbers = [int(n) for n in puzzle_input[0].split(",")]
+print(f"Numbers that will be drawn: {numbers}")
+
+board_strings = "\n".join(puzzle_input[1:]).split("\n\n")
+
+# Creating 2D int list with all numbers on the board
+boards = []
+marks = []
+
+# Creating boards and marks
+for board in board_strings:
+    boards.append(np.array([int(n) for n in board.replace("\n", " ").split(" ") if n.strip()], dtype=int).reshape(5, 5))
+    marks.append(np.zeros(shape=(5, 5), dtype=bool))
+
+victorious_boards = []
+
+# Playing until we found a winner
+for number in numbers:
+    print(f"Current number: {number}")
+
+    for board in range(len(boards)):
+
+        # Skipping boards that already won
+        if board in victorious_boards:
+            continue
+
+        # Marking matches
+        for match in np.column_stack(np.where(boards[board] == number)):
+            marks[board][match[0]][match[1]] = True
+
+        # Checking for victory
+        row_win = [np.all(marks[board][i] == True) for i in range(5)]
+        col_win = [np.all(np.transpose(marks[board])[i] == True) for i in range(5)]
+
+        # A board won
+        if any(row_win) or any(col_win):
+
+            # Just one remaining board
+            if len(victorious_boards) == len(boards) - 1:
+            
+                # Getting sum of all non-marked entries
+                res = 0
+                for row in range(5):
+                    for col in range(5):
+                        if not marks[board][row][col]:
+                            res += boards[board][row][col]
+
+                print(f"Result: {res * number}")
+                sys.exit()
+            else:
+                victorious_boards.append(board)
+
+            
+            
+                        
+
